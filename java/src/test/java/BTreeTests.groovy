34dc3082ffa8f382d5927a00df7261985d2c1f40
Ron Gross <ron.gross@gmail.com>
2011-01-01 17:51:27 +0200

    .gitignore

---
+++ java/src/test/java/BTreeTests.groovy
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ron
+ * Date: Jan 1, 2011
+ * Time: 5:47:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BTreeTests {
+  @Test
+  public void foo() {
+
+  }
+}
