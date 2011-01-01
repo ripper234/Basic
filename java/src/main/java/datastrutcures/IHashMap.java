fe27e684c6f254de2bd8669cbb442888a60d623e
Ron Gross <ron.gross@gmail.com>
2011-01-01 14:21:48 +0200

    Implemented HashMap

---
+++ java/src/main/java/datastrutcures/IHashMap.java
@@ -0,0 +1,9 @@
+package datastrutcures;
+
+public interface IHashMap<TKey, TValue> {
+    void put(TKey key, TValue value);
+    TValue get(TKey key);
+    boolean delete(TKey key);
+    void clear();
+    int size();
+}
